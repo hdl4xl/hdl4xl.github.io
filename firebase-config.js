9df3c6b056c6950589e760b24b6a605785ff4433
hdl4xl <171677004+hdl4xl@users.noreply.github.com>
2026-05-19 00:10:07 +0800

    Configure Firebase web app

--- firebase-config.js
+++ firebase-config.js
@@ -1,8 +1,11 @@
 // Firebase web config is public by design. Security depends on Firestore rules.
 // After creating a Firebase web app, replace the empty strings below.
 window.FIREBASE_CONFIG = {
-  apiKey: "",
-  authDomain: "",
-  projectId: "",
-  appId: "",
+  apiKey: "AIzaSyAGjqMQi7kwC1KwJsikEXacTs9b26jgtm4",
+  authDomain: "hdl4xl-lab.firebaseapp.com",
+  projectId: "hdl4xl-lab",
+  storageBucket: "hdl4xl-lab.firebasestorage.app",
+  messagingSenderId: "1062493737659",
+  appId: "1:1062493737659:web:01cb0127a812f611fa8f9e",
+  measurementId: "G-S3WZ5S48B1",
 };
